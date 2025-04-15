20e3d57a5eaac96ab79f8d78ec105ced8a08fee5
Philipsabri <philipsabri@gmail.com>
2025-04-15 23:48:23 +0200

    feat: add home-manager programs to mac spotlight

--- system/mac.nix
+++ system/mac.nix
@@ -64,11 +64,19 @@
       };
     in
     pkgs.lib.mkForce ''
-      # Set up applications.
+      # Set up system applications.
       echo "setting up /Applications..." >&2
       rm -rf /Applications/Nix\ Apps
       mkdir -p /Applications/Nix\ Apps
       find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+      # Set up home-manager applications.
+      cd "${config.users.users.philip.home}/Applications/Home Manager Apps/"
+      find . -maxdepth 1 -type l -exec readlink '{}' + |
       while read -r src; do
           app_name=$(basename "$src")
           echo "copying $src" >&2
